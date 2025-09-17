2cc062033e57d2bdeee8e0aed1431cba3cf32f8b
Fensy <muhammedguler1661xxx@gmail.com>
2025-09-17 15:19:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import OpenAI from "openai";
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 5050;
+const port = process.env.PORT || 6060;
 
 app.use(cors());
 app.use(bodyParser.json());
